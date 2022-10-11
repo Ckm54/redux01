d9038dc0c46d967911603c2227491de39f1155d4
collo <mutaic84@gmail.com>
2022-10-11 11:02:24 +0300

    complete posts application create and read functionality

--- bulletin_posts/src/features/post/postsSlice.js
+++ bulletin_posts/src/features/post/postsSlice.js
@@ -105,10 +105,10 @@ const postsSlice = createSlice({
           post.date = sub(new Date(), {minutes: min++}).toISOString()
           post.reactions = {
             thumbsUp: 0,
-            hooray: 0,
+            wow: 0,
             heart: 0,
             rocket: 0,
-            eyes: 0
+            coffee: 0
           }
           return post
         });
@@ -120,14 +120,22 @@ const postsSlice = createSlice({
         state.error = action.error.message;
       })
       .addCase(addNewPost.fulfilled, (state, action) => {
+
+        const sortedPosts = state.posts.sort((a, b) => {
+          if(a.id > b.id) return 1
+          if(a.id < b.id) return -1
+          return 0
+        })
+        action.payload.id = sortedPosts[sortedPosts.length -1].id + 1;
+        
         action.payload.userId = Number(action.payload.userId)
         action.payload.date = new Date().toISOString();
         action.payload.reactions = {
           thumbsUp: 0,
-          hooray: 0,
+          wow: 0,
           heart: 0,
           rocket: 0,
-          eyes: 0
+          coffee: 0
         }
         console.log(action.payload);
         state.posts.push(action.payload)
