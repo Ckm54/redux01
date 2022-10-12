81b8b541967d817f97db5b6cdf44f1ca7a5acee9
collo <mutaic84@gmail.com>
2022-10-12 11:27:57 +0300

    add a users page

--- bulletin_posts/src/components/Header.jsx
+++ bulletin_posts/src/components/Header.jsx
@@ -8,6 +8,7 @@ const Header = () => {
         <ul>
           <li><Link to="/" >Home</Link></li>
           <li><Link to="post" >Create Post</Link></li>
+          <li><Link to="user" >Users</Link></li>
         </ul>
       </nav>
     </header>
--- bulletin_posts/src/features/users/usersSlice.js
+++ bulletin_posts/src/features/users/usersSlice.js
@@ -32,5 +32,6 @@ const usersSlice = createSlice({
 });
 
 export const selectAllUsers = (state) => state.users;
+export const selectUserById = (state, userId) => state.users.find(user => user.id === userId);
 
 export default usersSlice.reducer;
