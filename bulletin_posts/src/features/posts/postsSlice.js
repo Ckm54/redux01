effdc7cc6b1b61a2d63c7e35ae5517215e44596b
collo <mutaic84@gmail.com>
2022-10-12 09:41:34 +0300

    add edit post functionality

--- bulletin_posts/src/features/posts/postsSlice.js
+++ bulletin_posts/src/features/posts/postsSlice.js
@@ -55,7 +55,17 @@ export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPos
   } catch (error) {
     return error.message;
   }
-})
+});
+
+export const updatePost = createAsyncThunk('posts/updatePost', async(initialPost) => {
+  const { id } = initialPost;
+  try {
+    const response = await axios.put(`${POSTS_URL}/${id}`, initialPost)
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+});
 
 const postsSlice = createSlice({
   name: "posts",
@@ -139,6 +149,17 @@ const postsSlice = createSlice({
         }
         console.log(action.payload);
         state.posts.push(action.payload)
+      })
+      .addCase(updatePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log('Update could not complete');
+          console.log(action.payload);
+          return
+        }
+        const { id } = action.payload;
+        action.payload.date = new Date().toISOString();
+        const posts = state.posts.filter(post => post.id !== id);
+        state.posts = [...posts, action.payload];
       })
   }
 });
