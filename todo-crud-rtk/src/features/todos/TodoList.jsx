090d1a35029b5f42947c4df634ae37fb50cde336
collo <mutaic84@gmail.com>
2022-10-14 00:04:38 +0300

    display todos as a list

--- todo-crud-rtk/src/features/todos/TodoList.jsx
+++ todo-crud-rtk/src/features/todos/TodoList.jsx
@@ -51,8 +51,17 @@ const TodoList = () => {
     content = todos.map((todo) => (
       <article key={todo.id}>
         <div className="todo">
-          
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            id={todo.id}
+            onChange={() => updateTodo({ ...todo, completed: !todo.completed})}
+          />
+          <label htmlFor={todo.id}>{todo.title}</label>
         </div>
+        <button className="trash" onClick={() => deleteTodo({id: todo.id})}>
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
       </article>
     ))
   } else if (isError) {
